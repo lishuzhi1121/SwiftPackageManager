321f40c352ef87dbdeedf748822bbf4f70aacd0b
Sands <lishzh@youzu.com>
2017-04-19 16:14:17 +0800

    add input number of cards

--- CardsGame/Sources/CardsGame/main.swift
+++ CardsGame/Sources/CardsGame/main.swift
@@ -1,11 +1,24 @@
+import Foundation
 import DeckOfPlayingCardsKit
+import Rainbow
 
 var deck = Deck.standard52CardDeck()
 deck.shuffle()
 
-for _ in 0..<10 {
+print("How many cards do you want?(0-52)".green)
+
+let input = readLine() ?? ""
+
+guard !input.isEmpty else {
+    print("Your input is invalid.".red.bold)
+    exit(EX_USAGE)
+}
+
+let numberOfCards = Int(input) ?? 0
+
+for _ in 0..<numberOfCards {
     guard let card = deck.deal() else {
-        print("No more Cards!")
+        print("No more Cards!".yellow.bold)
         break
     }
     
--- CardsGame/Sources/PlayingCardKit/Suit.swift
+++ CardsGame/Sources/PlayingCardKit/Suit.swift
@@ -1,3 +1,5 @@
+import Rainbow
+
 // 扑克牌花色枚举
 public enum Suit: String {
     // 黑桃、红桃、方块、梅花
@@ -25,13 +27,13 @@ extension Suit: CustomStringConvertible {
     public var description: String {
         switch self {
         case .spades:
-            return "♠︎"
+            return "♠︎".black
         case .hearts:
-            return "♥︎"
+            return "♥︎".red
         case .diamonds:
-            return "♦︎"
+            return "♦︎".red
         case .clubs:
-            return "♣︎"
+            return "♣︎".black
         }
     }
 }
